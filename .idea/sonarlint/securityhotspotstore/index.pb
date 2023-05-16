6e119ad6845873578793d688f4a8311cc1c21d01
ankit <you@example.com>
2023-05-16 20:29:56 +0530

    Mode advancement in TextField.kt (Added Toast, Icons,keyBoardOptions)

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+q
+Aapp/src/main/java/com/example/compose1/ui/theme/ExpandableCard.kt,e\0\e0df572dddee84a4a247007fc23cf0d12d87903d
+g
+7app/src/main/java/com/example/compose1/ui/theme/Type.kt,b\6\b6b39d295f4ff86ce1f7a8eaab6bfb86db007847
--- app/src/main/java/com/example/compose1/ui/theme/TextField.kt
+++ app/src/main/java/com/example/compose1/ui/theme/TextField.kt
@@ -1,9 +1,21 @@
 package com.example.compose1.ui.theme
 
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -13,18 +25,34 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview(showBackground = true)
 fun TextField() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        MyScreen()
+    }
     Column(
         modifier = Modifier
             .fillMaxSize(),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+
         var text by remember {
             mutableStateOf("Type here...")
         }
@@ -35,7 +63,48 @@ fun TextField() {
             label = {
                 Text(text = "Title")
             },
-            singleLine = true
+            //singleLine = true
+            leadingIcon = {
+                IconButton(onClick = {
+
+                }) {
+                    Icon(
+                        imageVector = Icons.Filled.Email,
+                        contentDescription = null
+                    )
+                }
+            },
+            trailingIcon = {
+                IconButton(onClick = {
+                    Log.d("Icons Pressed", "Clicked")
+                }) {
+                    Icon(
+                        imageVector = Icons.Filled.Check, tint = Color.Green,
+                        contentDescription = null
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            )
         )
+    }
+}
+
+@Composable
+fun MyScreen() {
+    ShowToastButton()
+}
+
+@Composable
+fun ShowToastButton() {
+    val context = LocalContext.current
+    Button(
+        onClick = {
+            Toast.makeText(context, "This is a toast message", Toast.LENGTH_SHORT).show()
+        }
+    ) {
+        Text("Show Toast")
     }
 }
